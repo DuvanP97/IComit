afa85ebb7989086828667b27f1895daa706446f1
Duvan <duvan.a.pedroza.n@gmail.com>
2019-04-21 10:12:38 -0500

    Act

---
+++ js/theme.js
@@ -0,0 +1,15 @@
+;(function($){
+    "use strict"
+	
+	
+	var swiper = new Swiper('.swiper-container', {
+		autoplay: {
+			delay: 3000,
+		},
+		speed: 2000,
+		loop: true,
+    });
+	
+	
+
+})(jQuery)
